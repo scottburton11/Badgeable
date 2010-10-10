c59cedd29762897b5719b9b2eef7ad976683affb
Scott Burton <scottburton11@gmail.com>
2010-10-09 18:24:28 -0700

    changed DSL to Dsl

--- lib/badgeable/railtie.rb
+++ lib/badgeable/railtie.rb
@@ -2,7 +2,7 @@ module Badgeable
   class Railtie < ::Rails::Railtie
     initializer "badgeable.load_badges" do
       Badgeable::Config.badge_definitions = Pathname.new("#{Rails.root}/lib/badges.rb")
-      Badgeable::DSL.class_eval(File.read(Badgeable::Config.badge_definitions))
+      Badgeable::Dsl.class_eval(File.read(Badgeable::Config.badge_definitions))
     end
   end
 end
