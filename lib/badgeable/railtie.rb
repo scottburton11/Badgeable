86d0642c381081a0c1b3c9385a774940ac75cddf
Scott Burton <scottburton11@gmail.com>
2010-10-14 14:54:33 -0700

    load badging definition file using the config.to_prepare block, so that they are reloaded on every request in development mode

--- lib/badgeable/railtie.rb
+++ lib/badgeable/railtie.rb
@@ -1,7 +1,10 @@
 module Badgeable
   class Railtie < ::Rails::Railtie
-    initializer "badgeable.load_badges" do
+    initializer do
       Badgeable::Config.badge_definitions = Pathname.new("#{Rails.root}/lib/badges.rb")
+    end
+    
+    config.to_prepare "badgeable.load_badges" do
       Badgeable::Dsl.class_eval(File.read(Badgeable::Config.badge_definitions))
     end
   end
