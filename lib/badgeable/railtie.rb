814c0ad247a157130a09182a3d2ea9ebafbd92ea
Scott Burton <scott@chaione.com>
2012-03-31 15:37:19 -0700

    warn if the badges.rb can not be found

--- lib/badgeable/railtie.rb
+++ lib/badgeable/railtie.rb
@@ -1,11 +1,21 @@
 module Badgeable
   class Railtie < ::Rails::Railtie
+
+    def badge_definition_file
+      "#{Rails.root}/lib/badges.rb"
+    end
+
     initializer "badgeable.assign_badge_definitions_file" do
-      Badgeable::Config.badge_definitions = Pathname.new("#{Rails.root}/lib/badges.rb")
+      if File.exists?(badge_definition_file)
+        Badgeable::Config.badge_definitions = File.read(Pathname.new(badge_definition_file))
+      else
+        Rails.logger.warn "Badgeable badges.rb file not found; add your badge definitions to #{badge_definition_file}, or run `rails g badgeable:setup` to get started"
+        Badgeable::Config.badge_definitions = ""
+      end
     end
     
     config.to_prepare do
-      Badgeable::Dsl.class_eval(File.read(Badgeable::Config.badge_definitions))
+      Badgeable::Dsl.class_eval Badgeable::Config.badge_definitions
     end
   end
 end
