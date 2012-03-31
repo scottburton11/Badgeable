e09a0572c61994045175901c1a2cd4b8fa31f6c5
Scott Burton <scott@chaione.com>
2012-03-31 15:05:26 -0700

    require adapter to be set by name

--- badgeable.gemspec
+++ badgeable.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency "rspec", ">= 2.0.0.rc"
   s.add_development_dependency "cucumber"
   s.add_development_dependency "factory_girl", ">= 1.3.2"
+  s.add_development_dependency "rake", "~> 0.8"
   
   s.files        = `git ls-files`.split("\n")
   s.executables  = `git ls-files`.split("\n").map{|f| f =~ /^bin\/(.*)/ ? $1 : nil}.compact
--- lib/badgeable/adapters.rb
+++ lib/badgeable/adapters.rb
@@ -1,8 +1,23 @@
 module Badgeable
   module Adapters
+
+    def self.current
+      @current_adapter
+    end
+
+    def self.use(adapter)
+      @current_adapter = adapter
+    end
+
+    def self.supported_adapters
+      [:active_record, :mongoid]
+    end
+
     def self.connect(klass)
-      klass.send(:include, Badgeable::Adapters::MongoidAdapter) if defined?(Mongoid) && klass.include?(::Mongoid::Document)
-      klass.send(:include, Badgeable::Adapters::ActiveRecordAdapter) if defined?(ActiveRecord) && klass.superclass == ActiveRecord::Base
+      # klass.send(:include, Badgeable::Adapters::MongoidAdapter) if defined?(Mongoid) && klass.include?(::Mongoid::Document)
+      # klass.send(:include, Badgeable::Adapters::ActiveRecordAdapter) if defined?(ActiveRecord) && klass.superclass == ActiveRecord::Base
+      raise ArgumentError, "Badgeable needs a database adapter to work. Add one of the following to your Gemfile: #{Badgeable::Adapters.supported_adapters.map {|name| 'badgeable_' + name }}" unless Badgeable::Adapters.current && (Badgeable::Adapters.supported_adapters.include? Badgeable::Adapters.current)
+      klass.send(:include, Badgeable::Adapters.const_get(Badgeable::Adapters.current.titleize + "Adapter" ))
     end
   end
 end
