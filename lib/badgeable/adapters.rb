6869067fdba5ae8a97fd991cee6919b31f8b0df5
Scott Burton <scott@chaione.com>
2012-03-31 17:14:48 -0700

    :current_adapter is a class attribute; remove comments

--- lib/badgeable/adapters.rb
+++ lib/badgeable/adapters.rb
@@ -1,8 +1,8 @@
 module Badgeable
   module Adapters
 
-    def self.current
-      @current_adapter
+    class << self
+      attr_accessor :current_adapter
     end
 
     def self.use(adapter)
@@ -14,8 +14,6 @@ module Badgeable
     end
 
     def self.connect(klass)
-      # klass.send(:include, Badgeable::Adapters::MongoidAdapter) if defined?(Mongoid) && klass.include?(::Mongoid::Document)
-      # klass.send(:include, Badgeable::Adapters::ActiveRecordAdapter) if defined?(ActiveRecord) && klass.superclass == ActiveRecord::Base
       raise ArgumentError, "Badgeable needs a database adapter to work. Add one of the following to your Gemfile: #{Badgeable::Adapters.supported_adapters.map {|name| 'badgeable_' + name }}" unless Badgeable::Adapters.current && (Badgeable::Adapters.supported_adapters.include? Badgeable::Adapters.current)
       klass.send(:include, Badgeable::Adapters.const_get(Badgeable::Adapters.current.titleize + "Adapter" ))
     end
