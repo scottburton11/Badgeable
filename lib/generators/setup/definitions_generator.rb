adbdc19dd2d2866f5be34d04962a4c20d41f4678
Scott Burton <scott@chaione.com>
2012-03-31 17:14:09 -0700

    add link to badge definition docs

--- lib/generators/setup/definitions_generator.rb
+++ lib/generators/setup/definitions_generator.rb
@@ -2,7 +2,7 @@ module Badgeable
   module Generators
     class DefinitionsGenerator < Badgeable::Generators::Base
       def create_badge_definitions_file
-        lib "badges.rb", "# Define your badges here"
+        lib "badges.rb", "# Define your badges here; see https://github.com/scottburton11/Badgeable for details"
       end
     end
   end
